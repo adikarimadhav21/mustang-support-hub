b38e68656d45697e13723c07de6a5cb131364d78
adikarimadhav21 <adhikarimadhav21@gmail.com>
2024-03-16 10:47:13 -0500

    admin: addded rideshare  model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 from . import models
+
+@admin.register(models.RideShareModel)
+class RideShareAdmin(admin.ModelAdmin):
+    list_display = [
+        "start_location",
+        "end_location",
+        "datetime",
+        "available_seats",
+        "total_seats",
+        "contact",
+    ]
+    list_filter = ["start_location", "end_location"]
